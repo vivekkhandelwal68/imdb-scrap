0aadb8080c46c8b4933ca01cd403ad491d26731e
vkhandelwal <vivekkhandelwal68@gmail.com>
2021-06-02 12:11:05 +0530

    Adding Comments in app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import re
 from func import get_movie_db, close_array, get_response
 
 
-file_name = 'output/bayDB.json'
-main_url = 'https://www.imdb.com/name/nm0000881/?ref_=nv_sr_srsg_0'
-main_response = get_response(main_url)
+file_name = 'output/bayDB.json'  # output file name
+main_url = 'https://www.imdb.com/name/nm0000881/?ref_=nv_sr_srsg_0'  # Director URL
+main_response = get_response(main_url)  # Getting response for Director URL
 
-pattern = re.compile(r'(<b>)(.*)(\s)(.*?)(</b>)')
-entries = pattern.finditer(main_response.text)
+pattern = re.compile(r'(<b>)(.*)(\s)(.*?)(</b>)')  # Forming Pattern object
+entries = pattern.finditer(main_response.text)  # Extracting elements satisfying pattern
 
-movie_dict = {}
+movie_dict = {}  # Empty Director will be having {'MovieName' : 'Movie IMDB Link'}
 for text in entries:
     name = text.group(4).split('>')[1].split('<')[0]
     link = 'https://www.imdb.com' + text.group(2).split('"')[1]
@@ -18,17 +18,17 @@ for text in entries:
 print(movie_dict)
 
 try:
-    os.remove(file_name)
+    os.remove(file_name)  # Removing Output File if already exists
     print('File successfully deleted !!')
 except FileNotFoundError as e:
     print('No File Found !')
 
 try:
-    get_movie_db(movie_dict.values(), file_name)
+    get_movie_db(movie_dict.values(), file_name)  # Preparing Movie Database
 except Exception as e:
     raise e
 finally:
-    close_array(file_name)
+    close_array(file_name)  # Closing Json Array
 
 
 
